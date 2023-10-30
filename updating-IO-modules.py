4f3b05631aa1ea68d32f52e6126be93d8b12c5c6
Muhammad Fawdy Renardi Wahyu <87023372+fawdywahyu18@users.noreply.github.com>
2023-10-30 12:55:09 +0700

    Update updating-IO-modules.py
    Periodic update on updating-io script

--- updating-IO-modules.py
+++ updating-IO-modules.py
@@ -92,6 +92,7 @@ def cleaning_matrix(tabel_input=None):
     return cleaning_result
 
 # Updating IO
+# Cara Manual
 # Classical IPF Algorithm
 def ipf_update(M, u, v):
     
@@ -250,6 +251,8 @@ def creating_panel_df(list_tahun_input, kode_kk_cd):
 def target_col_row(matrix_update=None, panel_df=None, industry_name_input=None,
                    adjusment_vector=True):
     
+    # Masih dalam konstruksi!!!
+    
     # Fungsi ini hanya berlaku untuk Tabel Input-Output 17 Lapangan Usaha.
     # matrix_update: mat, matrix yang akan diupdate.
     # panel_df: dataframe, dataframe berbentuk panel hasil dari function creating_panel_df.
@@ -303,3 +306,262 @@ def target_col_row(matrix_update=None, panel_df=None, industry_name_input=None,
                    'v_target':v_target}
     
     return dict_result
+
+def io_analysis(clean_matrix=None, list_industry=None, 
+                input_industry_name=None,
+                delta_fd_input=None,
+                delta_va_input=None,
+                first_round_id=None):
+    
+    # clean_matrix = matrix_clean_papbar
+    # list_industry = industry_name_papbar
+    # input_industry_name = sektor_unggulan_lq_papbar[2]
+    # delta_fd_input = 1
+    # delta_va_input = 1
+    # first_round_id='Demand'
+    # first_round_id='Demand' or 'Supply'
+    
+    # Multiplier and Linkage Analysis
+    # Demand-Side Multiplier
+    indeks_bb = list_industry.index(input_industry_name)
+    delta_fd = pd.DataFrame([0] * len(clean_matrix['Vector Final Demand']))
+    vec_dfd = delta_fd.to_numpy(dtype=int)
+    vec_dfd[indeks_bb] = delta_fd_input #disesuaikan dengan nilai impor industri
+    new_output = np.matmul(clean_matrix['Matrix Leontief Inverse'], vec_dfd)
+    multiplier_demand = np.sum(new_output)
+    
+    # Supply Side Multiplier
+    delta_va = pd.DataFrame([0] * len(clean_matrix['Vector Value Add']))
+    vec_dva = delta_va.to_numpy(dtype=int)
+    vec_dva[indeks_bb] = delta_va_input #disesuaikan dengan tambahan value added idustri
+    new_output_supply = np.matmul(clean_matrix['Matrix G Inverse'], vec_dva)
+    multiplier_supply = np.sum(new_output_supply)
+    
+    # Linkage (Source: Miller and Blair)
+    # Backward Linkage
+    colsum_A = np.sum(clean_matrix['Matrix A'], axis=0)
+    rowsum_A = np.sum(clean_matrix['Matrix A'], axis=1)
+    nrow_A = clean_matrix['Matrix A'].shape[0]
+    # ncol_A = mat_A.shape[1]
+    denominator_BL = np.matmul(colsum_A, rowsum_A)
+    numerator_BL = nrow_A * colsum_A
+    backward_linkage = numerator_BL/denominator_BL # Normalized backward linkage
+    bl_j = backward_linkage[indeks_bb] # Normalized Direct Backward Linkage for [Industry_Name]
+    # Angka BL menunjukkan bahwa [Industry_Name] memliki kategori (above average atau below average)
+    # atau linkage yang kuat/lemah sebagai pembeli
+    
+    # Direct Backward Linkage
+    direct_bl_j = colsum_A[indeks_bb]
+    
+    # Indirect Backward Linkage
+    colsum_L = np.sum(clean_matrix['Matrix L'], axis=0)
+    total_bl_j = colsum_L[indeks_bb]
+    indirect_bl_j = total_bl_j - direct_bl_j
+    
+    
+    # Direct Forward Linkage
+    colsum_B = np.sum(clean_matrix['Matrix B'], axis=0)
+    rowsum_B = np.sum(clean_matrix['Matrix B'], axis=1)
+    nrow_B = clean_matrix['Matrix B'].shape[0]
+    denominator_FL = np.matmul(colsum_B, rowsum_B)
+    numerator_FL = nrow_B * colsum_B
+    forward_linkage = numerator_FL/denominator_FL # Normalized forward linkage
+    fl_j = forward_linkage[indeks_bb] # Normalized Direct Forward Linkage for [Industry_Name]
+    # Angka BL menunjukkan bahwa [Industry_Name] memliki kategori (above average/below average)
+    # atau linkage yang kuat/lemah sebagai suplier
+    
+    # Direct Forward Linkage
+    direct_fl_j = rowsum_B[indeks_bb]
+    
+    # Indirect Forward Linkage
+    rowsum_G = np.sum(clean_matrix['Matrix G'], axis=1)
+    total_fl_j = rowsum_G[indeks_bb]
+    indirect_fl_j = total_fl_j - direct_fl_j
+    
+    
+    # Indeks Daya Penyebaran dan Indeks Derajat Kepekaan
+    colsum_L = np.sum(clean_matrix['Matrix Leontief Inverse'], axis=0)
+    rowsum_L = np.sum(clean_matrix['Matrix Leontief Inverse'], axis=1)
+    sumcolsum_L = np.sum(colsum_L)
+    sumrowsum_L = np.sum(rowsum_L)
+    jumlah_industri = len(list_industry)
+    idk = (jumlah_industri/sumrowsum_L)*rowsum_L # indeks daya penyebaran
+    idp = (jumlah_industri/sumcolsum_L)*colsum_L # indeks derajat kepekaan
+    idp_j = idp[indeks_bb]
+    idk_j = idk[indeks_bb]
+    
+    # Industry Effect
+    list_result_multiplier_output = [np.round(multiplier_demand,2), 
+                                     np.round(multiplier_supply,2)]
+    initial_effect = 1
+    if first_round_id=='Demand':
+        sum_indikator = rowsum_A
+        indeks_multiplier = 0
+    elif first_round_id=='Supply':
+        sum_indikator = rowsum_B
+        indeks_multiplier = 1
+    first_round_effect = sum_indikator[indeks_bb]
+    industrial_support_effect = list_result_multiplier_output[indeks_multiplier] - initial_effect - first_round_effect
+    production_induced_effect = first_round_effect + industrial_support_effect
+    
+    
+    analysis_result = {
+        'Multiplier Output Demand': np.round(multiplier_demand,4),
+        'Multiplier Output Supply': np.round(multiplier_supply,4),
+        'Normalized Direct Backward Linkage': np.round(bl_j, 4),
+        'Indirect Backward Linkage': np.round(indirect_bl_j, 4),
+        'Direct Backward Linkage': np.round(direct_bl_j, 4),
+        'Total Backward Linkage' : np.round(total_bl_j, 4),
+        'Normalized Direct Forward Linkage': np.round(fl_j, 4),
+        'Indirect Forward Linkage': np.round(indirect_fl_j, 4),
+        'Direct Forward Linkage': np.round(direct_fl_j, 4),
+        'Total Forward Linkage' : np.round(total_fl_j, 4),
+        'Indeks Daya Penyebaran': np.round(idp_j, 4),
+        'Indeks Derajat Kepekaan': np.round(idk_j, 4),
+        'First Round Effect': np.round(first_round_effect, 4),
+        'Industrial Support Effect': np.round(industrial_support_effect, 4),
+        'Production Induced Effect': np.round(production_induced_effect, 4)
+        }
+    
+    df_analisis_io = pd.DataFrame(analysis_result, index=[0]).reset_index()
+    df_melt = pd.melt(df_analisis_io, id_vars=['index'], value_vars=df_analisis_io.columns)
+    list_rename = ['Nama Industri', 'Jenis Analisis', 'Nilai']
+    df_melt.columns = list_rename
+    df_melt['Nama Industri'] = input_industry_name
+
+    return df_melt
+
+# Updating old Matrix based on new matrix Z
+def updating_old_matrix(matZ_hasil_update_input=None, matrix_clean_old=None):
+    # matZ_hasil_update_input = matZ_hasil_update
+    # matrix_clean_old = matrix_clean
+    
+    matZ_hasil_update_input = matZ_hasil_update
+    matrix_clean_old = matrix_clean
+    
+    # Demand Side
+    mat_Z = matZ_hasil_update_input
+    vec_fd = matrix_clean_old['Vector Final Demand']
+    vec_output = np.sum(mat_Z, axis=1) + vec_fd
+    
+    # vec_output = np.sum(mat_Z, axis=1) + vec_fd
+    # mat_output = vec_output * np.identity(len(vec_output))
+    mat_output = vec_output * np.identity(len(vec_output))
+    inv_x = np.linalg.pinv(mat_output)
+    
+    mat_A = np.matmul(mat_Z, inv_x)
+    mat_L = np.identity(len(mat_A)) - mat_A
+    mat_L_inv = np.linalg.inv(mat_L)
+    
+    # Supply Side
+    mat_B = np.matmul(inv_x, mat_Z) # allocation coefficient
+    mat_G = np.identity(len(mat_B)) - mat_B
+    mat_G_inv = np.linalg.inv(mat_G) # the output inverse
+    vec_va = matrix_clean_old['Vector Value Add']
+    # vec_input = df_to_clean.iloc[-1, 3:max_col_mat].to_numpy(dtype=float)
+    
+    updating_result = {
+        'Matrix Z': mat_Z,
+        'Matrix A': mat_A,
+        'Matrix L': mat_L,
+        'Matrix Leontief Inverse': mat_L_inv,
+        'Matrix B': mat_B,
+        'Matrix G': mat_G,
+        'Matrix G Inverse': mat_G_inv,
+        'Vector Output': vec_output,
+        'Vector Value Add': vec_va,
+        'Vector Final Demand': vec_fd
+        }
+    
+    return updating_result
+
+
+
+# Running function
+# Hati2 matrix before update keubah sendiri mengikuti hasil update, perlu load ulang
+
+# Sulawesi Selatan
+# Load data
+file_name_52_sulsel = 'Data/Tabel Input-Output Provinsi Sulawesi Selatan Transaksi Domestik Atas Dasar Harga Produsen (52 Industri) 2016 (Juta Rupiah).xlsx'
+file_name_17_sulsel = 'Data/Tabel Input-Output Provinsi Sulawesi Selatan Transaksi Domestik Atas Dasar Harga Produsen (17 Lapangan Usaha) 2016 (Juta Rupiah).xlsx'
+
+df_io = pd.read_excel(file_name_17_sulsel)
+matrix_id = initial_identification(df_io)
+matrix_clean = cleaning_matrix(df_io)
+industry_name = matrix_id['Industry Name']
+
+
+# Mendefinisikan row dan col target
+matA_before_update = matrix_clean['Matrix A']
+matZ_before_update = matrix_clean['Matrix Z']
+
+
+kode_sulsel = ['7300']
+kode_kepri = ['2100']
+kode_papbar = ['9100']
+list_tahun = ['2016', '2020'] # tahun awal harus 2016, sedangkan tahun akhir harus setelag 2016
+
+pdrb_sulsel = creating_panel_df(list_tahun_input=list_tahun, kode_kk_cd=kode_sulsel)
+pdrb_sulsel_col = pdrb_sulsel.columns
+pdrb_sulsel['pertumbuhan'] = (pdrb_sulsel[pdrb_sulsel_col[-1]] - pdrb_sulsel[pdrb_sulsel_col[-2]])/pdrb_sulsel[pdrb_sulsel_col[-2]]
+pdrb_sulsel.to_excel(f'Data Export/Sulawesi Selatan/Pertumbuhan per Lapangan Usaha Sulsel {list_tahun[0]} ke {list_tahun[1]}.xlsx')
+
+
+u_targetA = target_col_row(matA_before_update, pdrb_sulsel, industry_name, adjusment_vector=False)['u_target']
+v_targetA = target_col_row(matA_before_update, pdrb_sulsel, industry_name, adjusment_vector=False)['v_target']
+
+u_targetZ = target_col_row(matZ_before_update, pdrb_sulsel, industry_name, adjusment_vector=False)['u_target']
+v_targetZ = target_col_row(matZ_before_update, pdrb_sulsel, industry_name, adjusment_vector=False)['v_target']
+
+
+hasil_updateA = updating_IO(matA_before_update, u_targetA, v_targetA, 10000)
+hasil_updateZ = updating_IO(matZ_before_update, u_targetZ, v_targetZ, 10000)
+
+
+indeks_min = hasil_updateA['Index Minimum']
+# Hasil indeks_min = 0 artinya metode classic ipf adalah yg terbaik
+# hasil indeks_min = 1 artinya metode factor ipf adalah yg terbaik
+# hasil indeks_min = 2 artinya metode ipfn dari github adalah yg terbaik
+
+matA_hasil_update = hasil_updateA['Matrix Result']
+matZ_hasil_update = hasil_updateZ['Matrix Result']
+
+matrix_clean_sulsel_updated = updating_old_matrix(matZ_hasil_update,
+                                                  matrix_clean)
+
+
+sektor_unggulan_lq = ['Jasa Kesehatan dan Kegiatan Sosial',
+                      'Jasa Pendidikan',
+                      'Pertanian, Kehutanan, dan Perikanan']
+
+sektor_unggulan_ss = ['Perdagangan Besar dan Eceran; Reparasi Mobil dan Sepeda Motor',
+                      'Konstruksi',
+                      'Informasi dan Komunikasi']
+
+analisis_io_sulsel1 = io_analysis(clean_matrix=matrix_clean_sulsel_updated,
+                                  list_industry=industry_name,
+                                  input_industry_name=sektor_unggulan_ss[0],
+                                  delta_fd_input=1,
+                                  delta_va_input=1,
+                                  first_round_id='Demand')
+
+analisis_io_sulsel2 = io_analysis(clean_matrix=matrix_clean_sulsel_updated,
+                                  list_industry=industry_name,
+                                  input_industry_name=sektor_unggulan_ss[1],
+                                  delta_fd_input=1,
+                                  delta_va_input=1,
+                                  first_round_id='Demand')
+
+analisis_io_sulsel3 = io_analysis(clean_matrix=matrix_clean_sulsel_updated,
+                                  list_industry=industry_name,
+                                  input_industry_name=sektor_unggulan_ss[2],
+                                  delta_fd_input=1,
+                                  delta_va_input=1,
+                                  first_round_id='Demand')
+
+df_concat = pd.concat([analisis_io_sulsel1, analisis_io_sulsel2, analisis_io_sulsel3],
+                      axis=0)
+
+df_concat.to_excel('Data Export/Sulawesi Selatan/Hasil Update Analisis IO sektor unggulan SS Dinamis Sulawesi Selatan.xlsx',
+                   index=False)
+
