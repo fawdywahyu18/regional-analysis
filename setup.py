cdac7afce90432965cd815615e51c3280e603248
Muhammad Fawdy Renardi Wahyu <87023372+fawdywahyu18@users.noreply.github.com>
2023-12-07 10:53:14 +0700

    Create setup.py
    Membuat script untuk setup instalasi package regional-analysis

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='regional-analysis',
+    version='0.1.0',
+    py_modules=['regional-analysis'],
+    install_requires=[
+        'et-xmlfile==1.1.0',
+        'ipfn==1.4.4',
+        'numpy==1.26.2',
+        'openpyxl==3.0.10',
+        'pandas==1.5.2',
+        'python-dateutil==2.8.2',
+        'pytz==2023.3.post1',
+        'setuptools==68.0.0',
+        'six==1.16.0',
+        'wheel==0.41.2'
+    ],
+    entry_points='''
+        [console_scripts]
+        regional-analysis=regional-analysis:regional-analysis
+    ''',
+)
